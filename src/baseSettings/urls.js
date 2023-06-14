d1e334cde49cf659eab8fb4e67bbf184ea6ceaaf
Ivan Roschin <ivan.roschin86@gmail.com>
2023-06-15 00:37:52 +0300

    link

--- src/baseSettings/urls.js
+++ src/baseSettings/urls.js
@@ -1,5 +1,5 @@
-// const HOST_URL = 'https://wallet-backend-mu.vercel.app/api/';
-const HOST_URL = 'http://localhost:3030/api/';
+const HOST_URL = 'https://wallet-backend-mu.vercel.app/api/';
+// const HOST_URL = 'http://localhost:3030/api/';
 
 // const LOCALHOST_URL = 'http://localhost:3030/api/';
 
