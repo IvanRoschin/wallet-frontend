f017afc6a1b34f09ce961034f4ea7dfb2593419d
Ivan Roschin <ivan.roschin86@gmail.com>
2023-07-06 19:35:54 +0300

    update google auth

--- src/redux/auth/authSlice.jsx
+++ src/redux/auth/authSlice.jsx
@@ -14,6 +14,7 @@ export const AuthSlice = createSlice({
   },
   reducers: {
     setToken(state, action) {
+      console.log('action.payload', action.payload);
       state.accessToken = action.payload;
       state.isLoggedIn = true;
     },
